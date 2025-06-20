ccd81855d17937c1c5880cfd6d92b9f07a1eefb8
zibuyin <natdrone101@gmail.com>
2025-06-20 13:12:23 +0100

    Initial commit

---
+++ script/background.js
@@ -0,0 +1,20 @@
+const GOOGLE_ORIGIN = 'https://www.google.com';
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    if (!tab.url) return;
+    const url = new URL(tab.url);
+    // Enables the side panel on google.com
+    if (url.origin === GOOGLE_ORIGIN) {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            path: '/sidePanel/sidepanel.html',
+            enabled: true
+        });
+    } else {
+        // Disables the side panel on all other sites
+        await chrome.sidePanel.setOptions({
+            tabId,
+            enabled: false
+        });
+    }
+});
