50a1d7343b69e40fa5c8afd53fa3232958382c31
J4mik <145279407+J4mik@users.noreply.github.com>
2025-06-20 13:23:28 +0100

    background.js

--- script/background.js
+++ script/background.js
@@ -1,20 +1,4 @@
-const GOOGLE_ORIGIN = 'https://www.google.com';
+async function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
-chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-    if (!tab.url) return;
-    const url = new URL(tab.url);
-    // Enables the side panel on google.com
-    if (url.origin === GOOGLE_ORIGIN) {
-        await chrome.sidePanel.setOptions({
-            tabId,
-            path: '/sidePanel/sidepanel.html',
-            enabled: true
-        });
-    } else {
-        // Disables the side panel on all other sites
-        await chrome.sidePanel.setOptions({
-            tabId,
-            enabled: false
-        });
-    }
-});
