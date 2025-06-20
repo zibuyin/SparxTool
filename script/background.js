4b064a6f6446e1c013ede43526d9b7385b882945
J4mik <145279407+J4mik@users.noreply.github.com>
2025-06-20 13:39:44 +0100

    background.js

--- script/background.js
+++ script/background.js
@@ -1,4 +1,7 @@
-async function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
+// async function wait(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+    console.log(tabId, changeInfo, tab);
+  });
