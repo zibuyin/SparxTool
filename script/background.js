7da388ac2b186cd73deaea198e430816a222b586
J4mik <j4mikl@gmail.com>
2025-06-21 20:09:27 +0100

    backgroud

--- script/background.js
+++ script/background.js
@@ -2,10 +2,19 @@
 //     return new Promise(resolve => setTimeout(resolve, ms));
 // }
 
-chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-    console.log(tabId, changeInfo, tab);
-});
+// chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+//     console.log(tabId, changeInfo, tab);
+// });
 
-chrome.webNavigation.onCompleted.addListener(function () {
-    alert("This is my favorite website!");
-}, { url: [{ urlMatches: 'https://www.google.com/' }] });
+// chrome.webNavigation.onCompleted.addListener(function () {
+//     alert("This is my favorite website!");
+// }, { url: [{ urlMatches: 'https://www.google.com/' }] });
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.contextMenus.create({
+//       "id": "sampleContextMenu",
+//       "title": "Sample Context Menu",
+//       "contexts": ["selection"]
+//     });
+//   });
+console.log("running")
