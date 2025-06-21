21d0cfdfc2913a7c45feeb625ba42bbed59ff7d7
J4mik <j4mikl@gmail.com>
2025-06-21 20:30:51 +0100

    tab id

--- script/background.js
+++ script/background.js
@@ -1,20 +1,3 @@
-// async function wait(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-// chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-//     console.log(tabId, changeInfo, tab);
-// });
-
-// chrome.webNavigation.onCompleted.addListener(function () {
-//     alert("This is my favorite website!");
-// }, { url: [{ urlMatches: 'https://www.google.com/' }] });
-
-// chrome.runtime.onInstalled.addListener(function() {
-//     chrome.contextMenus.create({
-//       "id": "sampleContextMenu",
-//       "title": "Sample Context Menu",
-//       "contexts": ["selection"]
-//     });
-//   });
-console.log("running")
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log(tabs[0].id)
+});
