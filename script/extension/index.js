17b657669ae1314d7aa421cb1c8a66ac90901d9b
zibuyin <natdrone101@gmail.com>
2025-07-03 09:51:31 +0100

    added reference

---
+++ script/extension/index.js
@@ -0,0 +1,1 @@
+(function(){async function n(c,t=!1){const e=document.createElement("script");e.src=c,e.async=t,(document.head||document.body||document.documentElement).appendChild(e)}n(window.chrome.runtime.getURL("main.js"))})();
